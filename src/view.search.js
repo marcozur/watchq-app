019b8ba0ff48de08c789a65edab9284398acbaf4
Florian Unternaehrer <justus.jonas@gmail.com>
2012-10-06 23:35:06 +0200

    list is preserved and directly appended to DOM (more control, less performance)

--- src/view.search.js
+++ src/view.search.js
@@ -20,44 +20,59 @@ define(function(require) {
       this.$el.html(template());
     },
 
+    moviesCache: {},
     search: function(e) {
       var query = this.$el.find('#txtSearch').val(),
           searchResult = movies.search(query),
-          list = this.$el.find("#list"),
-          fakeList = $('<ul>');
+          searchResultIds = _.map(searchResult, function(m) { return m.id; }),
+          list = this.$el.find("#list");
 
-      // search local data
+      // remove items no longer valid from list, cache
+      _.each(_.keys(this.moviesCache), function(key) {
+        if (_.indexOf(searchResultIds, key) === -1) {
+          this.moviesCache[key].remove();
+          delete this.moviesCache[key];
+        }
+      }.bind(this));
+
+      // search local data, add items to results
       _.each(searchResult, function(movie) {
-        var li = $('<li>', { id:'movieItem'+movie.id, html: itemTemplate({ movie:movie }) });
-        fakeList.append(li);
-        // defer setting the thumbnail picture till its available
-        movie.getPosterSmallBase64(function(base64) {
-          setTimeout(function() {
-            $('#movieItem'+movie.id+' .thumb-80x').css({
-              'background': 'url(data:image/jpeg;base64,'+base64+')',
-              'opacity': 1
-            });
-          }, 200);
-        });
-      });
+        if (this.moviesCache[movie.id] === undefined) {
+          var li = $('<li>', { html:itemTemplate({ movie:movie }) });
 
+          // add to DOM, cache
+          list.append(li);
+          this.moviesCache[movie.id] = li;
+
+          // defer setting the thumbnail picture untill its available
+          movie.getPosterSmallBase64(function(base64) {
+            setTimeout(function() {
+              li.find('.thumb-80x').css({
+                'background': 'url(data:image/jpeg;base64,'+base64+')',
+                'opacity':1
+              });
+            }, 1);
+          });
+        }
+      }.bind(this));
+
+      // re-enhance list
+      list.listview("refresh");
+
+      // start online lookup on enter or after a specific time
       if (e !== undefined) {
-        // on [enter] lookup online
         if (e.keyCode === 13) {
+          // lookup on enter
           movies.lookup(query, this.search.bind(this));
         }
 
-        // on 0.51sec no input, lookup online
         setTimeout(function() {
+          // lookup online if query is still the same
           if (query === this.$el.find('#txtSearch').val()) {
             movies.lookup(query, this.search.bind(this));
           }
-        }.bind(this), 300);
+        }.bind(this), 650);
       }
-
-      // refresh list content
-      list.html(fakeList.html());
-      list.listview("refresh");
     }
 
   });
